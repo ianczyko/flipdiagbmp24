55ad54225d6b4cec39c763b46f984b5bfa671be8
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-05-14 21:50:00 +0200

    Added pixel_ptrs

--- flipdiagbmp24.asm
+++ flipdiagbmp24.asm
@@ -11,7 +11,30 @@ flipdiagbmp24:			        ; void flipdiagbmp24(void *img, int width);
         push    esi
         push    edi
 
-        mov     esi, [ebp+8]
+        ; padded width, pw = ((width * 3 + 3) // 4) * 4
+        mov     eax, [ebp+12]
+        lea     eax, [eax+eax*2]
+        add     eax, 3
+        shr     eax, 2
+        shl     eax, 2
+        mov     [ebp-4], eax
+
+        ; calculate left_pixel_ptr
+        mov     eax, [ebp+12]
+        sub     eax, 1
+        mul     dword [ebp-4]
+        mov     esi, eax
+
+        ; calculate right_pixel_ptr
+        mov     eax, [ebp+12]
+        mul     dword [ebp-4]
+        add     eax, 1
+        mov     edi, eax
+
+loop_row:
+
+loop_col:
+
 
         ; epilogue
         pop     edi
@@ -20,3 +43,10 @@ flipdiagbmp24:			        ; void flipdiagbmp24(void *img, int width);
         mov     esp, ebp
         pop     ebp
         ret
+
+; === variables ===
+; esi - left_pixel_ptr
+; edi - right_pixel_ptr
+;
+; [ebp+12] - width
+; [ebp-4] - padded width
