9c0b94c94bb7169ba4add62c7164dc0ef0825730
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-05-15 12:08:40 +0200

    Implemented ptr arithmetics for left_pixel_ptr

--- flipdiagbmp24.asm
+++ flipdiagbmp24.asm
@@ -24,7 +24,7 @@ flipdiagbmp24:			        ; void flipdiagbmp24(void *img, int width);
         sub     eax, 2
         mul     dword [ebp-4]
         mov     esi, eax
-        add     esi, [ebp+8]
+        ;add     esi, [ebp+8]           ; comment this line to make the ptr relative
 
         ; calculate right_pixel_ptr
         mov     eax, [ebp+12]
@@ -32,7 +32,7 @@ flipdiagbmp24:			        ; void flipdiagbmp24(void *img, int width);
         mul     dword [ebp-4]
         add     eax, 3
         mov     edi, eax
-        add     edi, [ebp+8]
+        ;add     edi, [ebp+8]           ; comment this line to make the ptr relative
 
         ; prepare row iteration counter
         mov     edx, [ebp+12]
@@ -42,12 +42,24 @@ loop_row:
         ; prepare column iteration counter
         mov     ecx, [ebp+12]
         sub     ecx, edx
-        
+
 loop_col:
+
+
+        ; update left_pixel_ptr
+        sub     esi, 3
 
         ; end loop_col
         dec     ecx
         jnz     loop_col
+
+        ; update left_pixel_ptr on row change
+        mov     eax, [ebp+12]
+        sub     eax, edx
+        add     eax, 1
+        sub     esi, [ebp-4]
+        lea     eax, [eax+eax*2]
+        add     esi, eax
 
         ; end loop_row
         dec     edx
